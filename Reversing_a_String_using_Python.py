cdc55765c08540b5f1e8fe075911ed8d2b8b5e8e
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-07-23 23:24:48 -0400

    Add project files.

---
+++ Reversing_a_String_using_Python.py
@@ -0,0 +1,11 @@
+﻿# الدالة لعكس سلسلة النص
+def reverse_string(s):
+    return s[::-1]
+
+# المثال
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+
+print("Original String:", input_string)
+print("Reversed String:", reversed_string)
+
